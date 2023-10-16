6b491b3d479b9f694ff5d1ec422163c5f7055ada
Siddhartha Aakash Rao <siddy0041@gmail.com>
2023-10-16 12:33:32 +0530

    extra code lines

--- main.py
+++ main.py
@@ -66,8 +66,18 @@ def draw_status():
 def check_win():
     global board, winner, draw
     for row in range(0,3):
-        if((board[row][0] == board[row][1] == board[row][2]) and (board[orw][0] is not None)):
+        if((board[row][0] == board[row][1] == board[row][2]) and (board[row][0] is not None)):
             winner = board[row][0]
             pg.draw.line(screen, (250, 0, 0),
                     (0, (row + 1)*height / 3 - height / 6),
-                    (width, (row
+                    (width, (row + 1)*height / 3 - height / 6),
+                    4)
+            break
+    for col in range(0,3):
+        if ((board[0][col] == board[1][col] == board[2][col])) and (board[0][col] is not None):
+            winner = board[0][col]
+            pg.draw.line(screen, (250, 0, 0), ((col + 1) * width / 3 - width / 6, 0),
+                         ((col + 1) * width / 3 - width / 6, height), 4)
+            break
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0],[0] is not None):
+        winner = board[0][0]
